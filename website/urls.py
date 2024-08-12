c044851f8ef6ee30344f633da4ec894d83509ac5
Mayank <mayankcryptobyte@gmail.com>
2024-08-13 00:42:38 +0530

    'fourth'

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Hello World')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home')
 ]
 
 urlpatterns += staticfiles_urlpatterns()
